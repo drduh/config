703121b20d1f1b7ae29c82e3b266ef714a74d8b6
drduh <doc@duh.to>
2018-07-18 22:12:52 -0700

    Drop ICMP unreachable messages

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# https://github.com/drduh/config/blob/master/firewall.sh
 
 PATH='/sbin'
 
@@ -11,7 +13,7 @@ INT_NET=172.16.1.0/24
 DMZ_NET=10.8.1.0/24
 WIFI_NET=192.168.1.0/24
 
-echo "Flush rules"
+echo "Flushing rules"
 iptables -F
 iptables -t nat -F
 iptables -t mangle -F
@@ -31,10 +33,12 @@ iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A OUTPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A FORWARD -m conntrack --ctstate INVALID -j DROP
 
-echo "Allow established, related and ICMP echo packets"
+echo "Allow established and related packets"
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+echo "Allow ICMP echo 'ping' replies"
 iptables -A INPUT -p icmp -m icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
 
 echo "Allow DHCP"
@@ -45,7 +49,7 @@ iptables -I INPUT -i $WIFI -p udp -m udp --dport 67 -m conntrack --ctstate NEW -
 echo "Allow SSH from local Ethernet"
 iptables -A INPUT -i $INT -s $INT_NET -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 
-echo "Allow DNS (TCP and UDP)"
+echo "Allow DNS (UDP and TCP for large replies)"
 iptables -A INPUT -i $INT -s $INT_NET -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $INT -s $INT_NET -p tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $DMZ -s $DMZ_NET -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
@@ -53,7 +57,7 @@ iptables -A INPUT -i $DMZ -s $DMZ_NET -p tcp --dport 53 -m conntrack --ctstate N
 iptables -A INPUT -i $WIFI -s $WIFI_NET -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $WIFI -s $WIFI_NET -p tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 
-echo "Add transparent Privoxy forwarding"
+echo "Add transparent Privoxy intercept"
 iptables -A INPUT -i $INT -s $INT_NET -p tcp --dport 8118 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t nat -A PREROUTING -i $INT -p tcp --dport 80 -j DNAT --to-destination 10.8.1.1:8118
 iptables -A INPUT -i $DMZ -s $DMZ_NET -p tcp --dport 8118 -m conntrack --ctstate NEW -j ACCEPT
@@ -78,7 +82,10 @@ iptables -A FORWARD -o $EXT -i $WIFI -s $WIFI_NET -m conntrack --ctstate NEW -j 
 # Un-comment to mangle TTL +1 (obscuring the fact packets were routed)
 #iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65
 
+echo "Do not reply with Destination Unreachable messages"
+iptables -A OUTPUT -p icmp --icmp-type destination-unreachable -j DROP
+
 echo "Log dropped packets"
-iptables -A INPUT -m limit --limit 1/sec -j LOG --log-level debug --log-prefix 'IN>'
-iptables -A OUTPUT -m limit --limit 1/sec -j LOG --log-level debug --log-prefix 'OUT>'
-iptables -A FORWARD -m limit --limit 1/sec -j LOG --log-level debug --log-prefix 'FWD>'
+iptables -A INPUT -m limit --limit 1/sec -j LOG --log-level debug --log-prefix 'DROPIN>'
+iptables -A OUTPUT -m limit --limit 1/sec -j LOG --log-level debug --log-prefix 'DROPOUT>'
+iptables -A FORWARD -m limit --limit 1/sec -j LOG --log-level debug --log-prefix 'DROPFWD>'
