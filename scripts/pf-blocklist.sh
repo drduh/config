650f8ad4d60c6164f55a480b6fa8df6b94a5c33e
drduh <github@duh.to>
2018-12-08 11:05:17 -0800

    Block twitter, update pf script

--- scripts/pf-blocklist.sh
+++ scripts/pf-blocklist.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 # https://github.com/drduh/config/blob/master/scripts/pf-blocklist.sh
+# Generates IP sets for autonomous systems and from published blocklists.
+# Facebook (AS32934)
+# Apple (AS714)
+# Microsoft (AS8075)
+# DoD (AS721)
+# Twitter (AS13414)
+rm ./pf-custom 2>/dev/null ; touch pf-custom
+for nb in AS32934 AS714 AS8075 AS721 AS13414 ; do
+  whois -h whois.radb.net !g$nb | \
+  tr " " "\n" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}.+' | \
+  sort | uniq >> pf-custom
+done
+wc -l pf-custom
+rm ./pf-threats 2>/dev/null ; touch pf-threats
 curl -sq \
   https://www.binarydefense.com/banlist.txt \
   https://rules.emergingthreats.net/blockrules/compromised-ips.txt \
@@ -10,5 +24,6 @@ curl -sq \
   https://isc.sans.edu/api/threatlist/shodan/shodan.txt | \
 grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | \
 grep -Ev "192.168.0.0|10.0.0.0|172.16.0.0|127.0.0.0|0.0.0.0" | \
-sort | uniq > pf-threats
+sort | uniq >> pf-threats
 wc -l pf-threats
+cat pf-custom pf-threats > pf-blacklist
