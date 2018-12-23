4661eecf6175e28bfc615c5da4e042ada4768c34
drduh <github@duh.to>
2018-12-23 10:18:49 -0800

    Fix openbsd update script

--- scripts/update-current.sh
+++ scripts/update-current.sh
@@ -4,21 +4,22 @@ host=cdn.openbsd.org
 plat=amd64
 vers=openbsd-64-base
 printf "current version: "
-ls -l /bsd.rd | awk '{print $6 " " $7}'
-printf "latest update: "
+stat -f "%Sm" -t "%d-%h-%Y" /bsd.rd
+printf "latest version: "
 curl -sq https://$host/pub/OpenBSD/snapshots/$plat/ | grep "bsd.rd" | awk '{print $3}'
 action=""
 while [[ -z "${action}" ]] ;
   do read -n 1 -p "Proceed? " action
 done
+printf "\n"
 if [[ "${action}" =~ ^([yY])$ ]] ; then
-  cp bsd.rd -v bsd.rd.$(date +%F) 2>/dev/null
+  doas cp /bsd.rd ~/bsd.rd.$(date +%F)
   curl -LfsqO https://$host/pub/OpenBSD/snapshots/$plat/bsd.rd && \
   curl -LfsqO https://$host/pub/OpenBSD/snapshots/$plat/SHA256 && \
   curl -LfsqO https://$host/pub/OpenBSD/snapshots/$plat/SHA256.sig && \
   signify -Cp /etc/signify/$vers.pub -x SHA256.sig bsd.rd && \
-  doas cp -v ./bsd.rd /bsd.rd && doas chown root:wheel /bsd.rd
+  doas cp -v ./bsd.rd /bsd.rd && doas chown root:wheel /bsd.rd && \
+  echo "reboot into /bsd.rd and select upgrade"
 else
   exit 0
 fi
-echo "reboot into /bsd.rd to upgrade"
