3b03b3892adc603fdbaed629cce8ed1751413ea9
drduh <github@duh.to>
2018-12-29 18:43:32 -0800

    Improve openbsd update script

--- scripts/update-current.sh
+++ scripts/update-current.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 # https://github.com/drduh/config/blob/master/scripts/update-current.sh
+set -e
 host=cdn.openbsd.org
 plat=amd64
 vers=openbsd-64-base
 printf "current version: "
 stat -f "%Sm" -t "%d-%h-%Y" /bsd.rd
 printf "latest version: "
-curl -sq https://$host/pub/OpenBSD/snapshots/$plat/ | grep "bsd.rd" | awk '{print $3}'
+curl -sq \
+  https://$host/pub/OpenBSD/snapshots/$plat/ | grep "bsd.rd" | awk '{print $3}'
 action=""
 while [[ -z "${action}" ]] ;
-  do read -n 1 -p "Proceed? " action
+  do read -n 1 -p "Continue? (y/n) " action
 done
 printf "\n"
 if [[ "${action}" =~ ^([yY])$ ]] ; then
   doas cp /bsd.rd ~/bsd.rd.$(date +%F)
-  curl -LfsqO https://$host/pub/OpenBSD/snapshots/$plat/bsd.rd && \
-  curl -LfsqO https://$host/pub/OpenBSD/snapshots/$plat/SHA256 && \
-  curl -LfsqO https://$host/pub/OpenBSD/snapshots/$plat/SHA256.sig && \
+  curl -sq \
+  -O https://$host/pub/OpenBSD/snapshots/$plat/bsd.rd \
+  -O https://$host/pub/OpenBSD/snapshots/$plat/SHA256 \
+  -O https://$host/pub/OpenBSD/snapshots/$plat/SHA256.sig && \
   signify -Cp /etc/signify/$vers.pub -x SHA256.sig bsd.rd && \
   doas mv -v ./bsd.rd /bsd.rd && doas chown root:wheel /bsd.rd && \
-  echo "reboot into /bsd.rd and select upgrade"
+  printf "reboot to /bsd.rd and select Upgrade"
 else
   exit 0
 fi
