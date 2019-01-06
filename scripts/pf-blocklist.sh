f27aae1aca5437506eb398b4d4194a380a344e1e
drduh <github@duh.to>
2019-01-05 23:18:55 -0800

    Block more addresses with pf

--- scripts/pf-blocklist.sh
+++ scripts/pf-blocklist.sh
@@ -1,38 +1,68 @@
 #!/usr/bin/env bash
 # https://github.com/drduh/config/blob/master/scripts/pf-blocklist.sh
-# Generates IP sets for autonomous systems and from published blocklists.
-# Apple (AS714)
-# Facebook (AS32934)
-# Microsoft (AS8075)
-# Twitter (AS13414)
-# DoD (AS721)
-custom=pf-custom-rules.$(date +%F)
+custom=pf-custom.$(date +%F)
 threats=pf-threats.$(date +%F)
+zones=pf-zones.$(date +%F)
 blocklist=pf-blocklist.$(date +%F)
-rm $custom $threats $blocklist 2>/dev/null
-for nb in AS32934 AS714 AS8075 AS721 AS13414 ; do
-  whois -h whois.radb.net !g$nb | \
-  tr " " "\n" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}.+' | \
-  sort | uniq >> $custom
-done
-wc -l $custom
-curl -sq \
-  https://www.binarydefense.com/banlist.txt \
-  https://rules.emergingthreats.net/blockrules/compromised-ips.txt \
-  https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt \
-  https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level3.netset \
-  https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset \
-  https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset \
-  https://isc.sans.edu/api/threatlist/shodan/shodan.txt | \
-grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | \
-grep -Ev "192.168.0.0|10.0.0.0|172.16.0.0|127.0.0.0|0.0.0.0" | \
-sort | uniq > $threats
-wc -l $threats
-cat $threats $custom | sort | uniq > $blocklist
-doas cp -v /etc/pf/blocklist /etc/pf/blocklist.old
-doas cp -v ./pf-blocklist /etc/pf/blocklist
-doas pfctl -d ; doas pfctl -e -f /etc/pf.conf
+
+doas whoami
+
+printf "current rules: "
 doas pfctl -t blocklist -T show | wc -l
-for s in apple.com facebook.com microsoft.com twitter.com ; do
-  curl -vv https://$(dig a $s @1.1.1.1 +short|head -n1) 2>&1|grep "Permission denied"
+action=""
+while [[ -z "${action}" ]] ;
+  do read -n 1 -p "Continue? (y/n) " action
 done
+printf "\n"
+if [[ "${action}" =~ ^([yY])$ ]] ; then
+  doas rm $custom $threats $zones $blocklist 2>/dev/null
+
+  curl -sq \
+    https://www.binarydefense.com/banlist.txt \
+    https://rules.emergingthreats.net/blockrules/compromised-ips.txt \
+    https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt \
+    https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level{1..3}.netset \
+    https://isc.sans.edu/api/threatlist/shodan/shodan.txt | \
+  grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | \
+  grep -Ev "192.168.0.0|10.0.0.0|172.16.0.0|127.0.0.0|0.0.0.0" > $threats
+  wc -l $threats
+
+  # Apple (AS714)
+  # Facebook (AS32934)
+  # Microsoft (AS8075)
+  # Twitter (AS13414)
+  # DoD (AS721)
+  # Note this returns blocks of addresses - not individual IPs!
+  rm $custom ; touch $custom
+  for nb in AS714 AS32934 AS8075 AS13414 AS721 ; do
+    whois -h whois.radb.net !g$nb | \
+    tr " " "\n" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}.+' | \
+    sort | uniq >> $custom
+  done
+  wc -l $custom
+
+  touch $zones
+  for zn in ae af az bd cd cg cn eg gh ht iq ir jo ke kw kz lr ly me mk mn mv mx na ne ng ni nr pk pl pr qa ru rw sa sd sr sy td tj tn tr tt ua ug uz ve ye zm ; do
+    curl -sq \
+      http://www.ipdeny.com/ipblocks/data/countries/$zn.zone >> $zones
+  done
+  wc -l $zones
+
+  sort $threats $custom $zones | uniq > $blocklist
+  wc -l $blocklist
+
+  doas cp -v /etc/pf/blocklist /etc/pf/blocklist.$(date +%F)
+  doas cp -v $blocklist /etc/pf/blocklist
+  doas pfctl -d ; doas pfctl -e -f /etc/pf.conf
+
+  printf "new rules: "
+  doas pfctl -t blocklist -T show | wc -l
+else
+  printf "testing blocked sites ..."
+  for ws in apple.com facebook.com microsoft.com twitter.com ; do
+    printf "\n$ws: "
+    curl -v \
+      https://$(dig a $ws @1.1.1.1 +short | head -n1) 2>&1 | grep "Permission denied" || printf "BLOCK FAILED"
+  done
+fi
+
