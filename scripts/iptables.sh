2dc0150c2d0a57e924f5cb8ad903b8af82de3aa7
drduh <github@duh.to>
2019-06-09 15:23:43 -0700

    Sync

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -28,11 +28,11 @@ echo "Drop invalid states"
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A OUTPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A FORWARD -m conntrack --ctstate INVALID -j DROP
-echo "Allow established and related packets"
+echo "Allow established and related connections"
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-echo "Allow echo 'ping' replies"
+echo "Allow ping replies"
 iptables -A INPUT -p icmp -m icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
 echo "Allow DHCP"
 iptables -I INPUT -i $INT -p udp -m udp --dport 67 -m conntrack --ctstate NEW -j ACCEPT
@@ -51,25 +51,24 @@ iptables -A INPUT -i $DMZ -s $DMZ_NET -p udp --dport 53 -m conntrack --ctstate N
 iptables -A INPUT -i $DMZ -s $DMZ_NET -p tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $WIFI -s $WIFI_NET -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $WIFI -s $WIFI_NET -p tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
-echo "Add transparent Privoxy intercept"
+echo "Intercept HTTP traffic to Privoxy"
 iptables -A INPUT -i $INT -s $INT_NET -p tcp --dport 8118 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $DMZ -s $DMZ_NET -p tcp --dport 8118 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i $WIFI -s $WIFI_NET -p tcp --dport 8118 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t nat -A PREROUTING -i $INT -p tcp --dport 80 -j DNAT --to-destination 10.8.1.1:8118
 iptables -t nat -A PREROUTING -i $DMZ -p tcp --dport 80 -j DNAT --to-destination 172.16.1.1:8118
 iptables -t nat -A PREROUTING -i $WIFI -p tcp --dport 80 -j DNAT --to-destination 192.168.1.1:8118
-echo "Allow outgoing to Internet"
+echo "Allow outgoing on external interface"
 iptables -A OUTPUT -o $EXT -d 0.0.0.0/0 -j ACCEPT
-echo "Allow traffic from the firewall to LAN"
+echo "Allow traffic from the firewall to local networks"
 iptables -A OUTPUT -o $INT -d $INT_NET -j ACCEPT
 iptables -A OUTPUT -o $DMZ -d $DMZ_NET -j ACCEPT
 iptables -A OUTPUT -o $WIFI -d $WIFI_NET -j ACCEPT
-echo "Enable NAT"
+echo "Enable network address translation"
 iptables -t nat -A POSTROUTING -o $EXT -j MASQUERADE
 iptables -A FORWARD -o $EXT -i $INT -s $INT_NET -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -o $EXT -i $DMZ -s $DMZ_NET -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -o $EXT -i $WIFI -s $WIFI_NET -m conntrack --ctstate NEW -j ACCEPT
-# Un-comment to mangle TTL +1 (obscuring the fact packets were routed)
 #iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65
 echo "Do not reply with Destination Unreachable messages"
 iptables -A OUTPUT -p icmp --icmp-type destination-unreachable -j DROP
