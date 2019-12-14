2ce0dc4c0626f5002a1bc461ee9c179cb27285fc
drduh <github@duh.to>
2019-12-14 12:22:32 -0800

    Check-in coreboot x230 config

--- scripts/split-rom.sh
+++ scripts/split-rom.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # https://github.com/drduh/config/blob/master/scripts/split-rom.sh
-
-cd util/nvramtool
-./nvramtool -C ../../build/coreboot.rom -w gfx_uma_size=224M
-./nvramtool -C ../../build/coreboot.rom -w bluetooth=Disable
-./nvramtool -C ../../build/coreboot.rom -w wwan=Disable
-./nvramtool -C ../../build/coreboot.rom -w wlan=Disable
-
-cd ../../build
-rm -fv coreboot-bottom.rom coreboot-top.rom
+cd build/util/nvramtool
+./nvramtool -C ../../coreboot.rom -w gfx_uma_size=224M
+./nvramtool -C ../../coreboot.rom -w wwan=Disable
+./nvramtool -C ../../coreboot.rom -w wlan=Disable
+./nvramtool -C ../../coreboot.rom -w bluetooth=Disable
+cd ../..
+rm -f coreboot-bottom.rom coreboot-top.rom
 dd if=coreboot.rom of=coreboot-bottom.rom bs=1M count=8
 dd if=coreboot.rom of=coreboot-top.rom bs=1M skip=8
+mkdir -p ~/build/coreboot/write/$(date +%F)
+mv build/*.rom ~/build/coreboot/write/$(date +%F)
