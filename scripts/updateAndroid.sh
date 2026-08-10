3a6d729ae67a9acbf4403fdeb4ff2277d0097b4c
drduh <github@duh.to>
2026-08-09 18:30:00 -0700

    curl array, check git rate limit

--- scripts/updateAndroid.sh
+++ scripts/updateAndroid.sh
@@ -8,8 +8,9 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
-apksigner="$(command -v apksigner || command -v \
-  ${HOME}/Library/Android/sdk/build-tools/36.1.0/apksigner)"
+apksigner="$(command -v apksigner ||
+  command -v ${HOME}/Library/Android/sdk/build-tools/37.0.0/apksigner ||
+  command -v ${HOME}/build/apksigner)"
 
 certAegis="c6db80a8e14e5230c1de8415ef820d13dc901d8fe33cf3acb57b6862d858a823"
 certFairEmail="e02067249f5a350e0ec703fe9df4dd682e0291a09f0c2e041050bbe7c064f5c9"
@@ -21,11 +22,13 @@ certNewPipe="cb84069bd68116bafae5ee4ee5b08a567aa6d898404e7cb12f9e756df5cf5cab"
 certOsmand="d192f4fffff2fae37f2821e4ca44f4cbe2483e7ffa24a8472043f685dd5bed27"
 certProton="dcc9439ec1a6c6a8d0203f3423ee42bcc8b970628e53cb73a0393f398dd5b853"
 certSignal="4be4f6cd5be844083e900279dc822af65a547fecc26aba7ff1f5203a45518cd8"
-# older 1024-bit signing certificate
+# legacy 1024-bit signing certificate
 #certSignal="29f34e5f27f211b424bc5bf9d67162c0eafba2da35af35c16416fc446276ba26"
 certWireGuard="5204d82e766e8aa14dcbb06dc70aebae2bdd812d4d6203cd521a8a685d7d3d80"
 
-gitRepos="https://api.github.com/repos"
+gitBase="https://api.github.com"
+gitRateLimit="${gitBase}/rate_limit"
+gitRepos="${gitBase}/repos"
 gitRepoAegis="${gitRepos}/beemdevelopment/Aegis"
 gitRepoCalendar="${gitRepos}/FossifyOrg/Calendar"
 gitRepoFairEmail="${gitRepos}/M66B/FairEmail"
@@ -36,12 +39,26 @@ gitRepoNewPipe="${gitRepos}/TeamNewPipe/NewPipe"
 gitRepoNotes="${gitRepos}/FossifyOrg/Notes"
 gitRepoProton="${gitRepos}/ProtonMail/android-mail"
 gitRepoWireGuard="${gitRepos}/wgtunnel/android"
-gitUrl="https://github.com"
 
 urlFirefox="https://archive.mozilla.org/pub/fenix/releases"
 urlOsmand="https://download.osmand.net/releases"
 urlSignal="https://updates.signal.org/android/latest.json"
 
+userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:153.0) Gecko/20100101 Firefox/153.0"
+cmdCurl=(
+  curl
+  --proto =https
+  --tlsv1.2
+  --user-agent "$userAgent"
+  --connect-timeout 5
+  --silent
+  --show-error
+)
+
+warn() {
+  # Print error string in red and exit.
+  tput setaf 1 ; printf "WARN: %s\n" "${1}" ; tput sgr0
+}
 
 fail() {
   # Print error string in red and exit.
@@ -66,22 +83,28 @@ printValid() {
 
 download() {
   # Download and save file, or exit on error.
-  curl --connect-timeout 5 -sLfO "${1}" || \
+  "${cmdCurl[@]}" --location -O "${1}" ||
     fail "could not download '${1}'"
 }
 
 verify() {
   # Verify signature with apksigner or exit on fail.
-  "${apksigner}" verify --verbose --print-certs "${1}" 2>/dev/null | \
+  "${apksigner}" verify --verbose --print-certs "${1}" 2>/dev/null |
     grep -q "${2}" || fail "could not verify '${1}'"
 }
 
 getReleaseGit() {
   # Get and parse latest Git release information.
-  json=$(curl -s "${1}/releases/latest") || \
-    fail "could not get release"
+  json=$("${cmdCurl[@]}" "${1}/releases/latest" 2>&1)
+  if [[ "$json" == *"403"* ]] ; then
+    fail "request was denied"
+  fi
+  if [[ "$json" == *"rate limit exceeded"* ]] ; then
+    fail "request was rate limited"
+  fi
   if ! jq -e '.assets | length > 0' >/dev/null <<< "${json}" ; then
-    fail "no release found" ; fi
+    fail "no release found"
+  fi
   jq -r '.assets[0] |
     [.uploader.login, .name, .browser_download_url,
     (.download_count|tostring), .updated_at] | @tsv' <<< "${json}"
@@ -91,6 +114,14 @@ getPackageGit() {
   # Download and verify latest Git release package.
   IFS=$'\t' read -r \
     author package url count mtime < <(getReleaseGit "${1}")
+  if [[ "$author" == *"rate limited"* ]] ; then
+    warn "failed to get ${1} release: $author"
+    return
+  fi
+  if [[ "$author" == *"denied"* ]] ; then
+    warn "failed to get ${1} release: $author"
+    return
+  fi
   printRelease "${package} (${author}@${mtime}, ${count} downloads)"
   if [[ ! -f "${package}" ]] ; then
     printLoad "${package}" ; download "${url}" ; fi
@@ -138,37 +169,34 @@ updateWireGuard() {
 }
 
 updateFirefox() {
-  version=$(curl -s "${urlFirefox}/" | \
+  version=$("${cmdCurl[@]}" "${urlFirefox}/" |
     grep -Eo "[0-9]{1,}.[0-9]{1,}.[0-9]{1,}" | grep -v b | sort -V | tail -1)
   apkPath="fenix-${version}-android-arm64-v8a"
   package="fenix-${version}.multi.android-arm64-v8a.apk"
   if [[ ! -f "${package}" ]] ; then
     printLoad "${package}"
-    curl -sLfO "${urlFirefox}/${version}/android/${apkPath}/${package}" || \
-      fail "could not download Firefox"
+    download "${urlFirefox}/${version}/android/${apkPath}/${package}"
   fi
   verify "${package}" "${certFirefox}" && printValid "${package}"
 }
 
 updateOsmand() {
-  package=$(curl -s "${urlOsmand}/" | \
-    grep -Eo "net.osmand-[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}.apk" | \
+  package=$("${cmdCurl[@]}" "${urlOsmand}/" |
+    grep -Eo "net.osmand-[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}.apk" |
       sort -V | tail -1)
   if [[ ! -f "${package}" ]] ; then
     printLoad "${package}"
-    curl -sLfO "${urlOsmand}/${package}" || \
-      fail "could not download '${package}'"
+    download "${urlOsmand}/${package}"
   fi
   verify "${package}" "${certOsmand}" && printValid "${package}"
 }
 
 updateSignal() {
-  version=$(curl -s "${urlSignal}" | jq -r '.url')
+  version=$("${cmdCurl[@]}" "${urlSignal}" | jq -r '.url')
   package=$(basename "${version}")
   if [[ ! -f "${package}" ]] ; then
     printLoad "${package}"
-    curl -sLfO "${version}" || \
-      fail "could not download '${package}'"
+    download "${version}"
   fi
   verify "${package}" "${certSignal}" && printValid "${package}"
 }
@@ -196,8 +224,9 @@ updateAllUrl() {
 
 checkConnection() {
   # Check connectivity to Git or exit.
-  curl -sI "${gitUrl}" >/dev/null || \
-    fail "could not reach '${gitUrl}'"
+  "${cmdCurl[@]}" ${gitRateLimit} |
+    jq -e '.rate.remaining == 0' >/dev/null &&
+    fail "github api rate limit"
 }
 
 main() {
